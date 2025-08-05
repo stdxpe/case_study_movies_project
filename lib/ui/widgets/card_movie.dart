4ac0bd31c650471b212713237a491af7f62fa5aa
Yasin Bilgin <stdxpe@gmail.com>
2025-08-05 23:33:36 +0300

    movie cards on profile created

---
+++ lib/ui/widgets/card_movie.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import 'package:case_study_movies_project/ui/widgets/text_custom.dart';
+import 'package:case_study_movies_project/utilities/utilities_library_imports.dart';
+
+class CardMovie extends StatelessWidget {
+  const CardMovie({
+    super.key,
+    required this.imagePath,
+    required this.movieTitle,
+    required this.movieSubtitle,
+  });
+
+  final String imagePath;
+  final String movieTitle;
+  final String movieSubtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(AppConstants.radius.movieCard),
+          child: AspectRatio(
+            aspectRatio: AppConstants.sizes.movieCardAspectRatio,
+            child: Image.network(
+              imagePath,
+              fit: BoxFit.cover,
+              alignment: Alignment.center,
+              height: AppConstants.sizes.movieCardHeight,
+              width: double.infinity,
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress == null) return child;
+                return Center(
+                  child: CircularProgressIndicator(
+                    value: loadingProgress.expectedTotalBytes != null
+                        ? loadingProgress.cumulativeBytesLoaded /
+                            loadingProgress.expectedTotalBytes!
+                        : null,
+                  ),
+                );
+              },
+              errorBuilder: (context, error, stackTrace) => const Center(
+                child: Icon(Icons.broken_image, size: 40, color: Colors.grey),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: AppConstants.spacings.space16),
+        TextCustom(
+          text: movieTitle,
+          textStyle: context.textTheme.infoLight,
+          fontWeightCustom: FontWeight.w500,
+          alignment: Alignment.centerLeft,
+          color: context.colorPalette.text,
+        ),
+        TextCustom(
+          text: movieSubtitle,
+          textStyle: context.textTheme.infoLight,
+          alignment: Alignment.centerLeft,
+          color: context.colorPalette.textFaded05,
+        ),
+      ],
+    );
+  }
+}
