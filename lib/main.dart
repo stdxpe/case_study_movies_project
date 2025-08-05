9c9cc88a42272d2344027293e759ee01ff9cb1db
Yasin Bilgin <stdxpe@gmail.com>
2025-08-05 18:16:19 +0300

    git tested

--- lib/main.dart
+++ lib/main.dart
@@ -49,9 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            const Text('GIT TESTED'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
@@ -61,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'TESTED!',
         child: const Icon(Icons.add),
       ),
     );
