dac9428578d7e38db9d10756c02d63e316c16343
Yasin Bilgin <stdxpe@gmail.com>
2025-08-05 23:32:49 +0300

    custom app bars created

---
+++ lib/ui/widgets/appbar_custom.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import 'package:case_study_movies_project/ui/widgets/button_lite.dart';
+import 'package:case_study_movies_project/ui/widgets/text_custom.dart';
+import 'package:case_study_movies_project/utilities/utilities_library_imports.dart';
+
+class AppBarCustom extends StatelessWidget implements PreferredSizeWidget {
+  final String titleText;
+  final VoidCallback onBackPressed;
+  final Widget? rightWidget;
+
+  const AppBarCustom({
+    super.key,
+    required this.titleText,
+    required this.onBackPressed,
+    this.rightWidget,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final sidePadding = AppConstants.paddings.appBar;
+
+    return SafeArea(
+      child: Container(
+        height: preferredSize.height,
+        padding: EdgeInsets.symmetric(horizontal: sidePadding),
+        color: Colors.transparent,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Center(
+              child: TextCustom(
+                text: titleText,
+                color: context.theme.colorPalette.text,
+                textStyle: context.textTheme.action,
+              ),
+            ),
+            Positioned(
+              left: 0,
+              child: ButtonLite(
+                iconPath: AppVisuals.leftArrow,
+                iconSize: AppConstants.sizes.iconLeftArrowHeight,
+                size: AppConstants.sizes.backButtonHeight,
+                radius: AppConstants.radius.circular,
+                onPressed: onBackPressed,
+              ),
+            ),
+            if (rightWidget != null)
+              Positioned(
+                right: 0,
+                child: rightWidget!,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(AppConstants.sizes.appBarHeight);
+}
---
+++ lib/ui/widgets/appbar_sliver_profile.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'package:case_study_movies_project/services/dummy_data.dart';
+import 'package:case_study_movies_project/ui/widgets/card_profile.dart';
+import 'package:case_study_movies_project/utilities/utilities_library_imports.dart';
+
+class AppbarSliverProfile extends StatelessWidget {
+  const AppbarSliverProfile({super.key, required this.innerBoxIsScrolled});
+
+  final bool innerBoxIsScrolled;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      shadowColor: context.colorPalette.scaffoldBackground,
+      surfaceTintColor: context.colorPalette.scaffoldBackground,
+      foregroundColor: context.colorPalette.scaffoldBackground,
+      pinned: false,
+      floating: true,
+      snap: true,
+      backgroundColor: context.colorPalette.scaffoldBackground,
+      expandedHeight: AppConstants.sizes.profilePicHeight,
+
+      /// TODO: Get UserModel dynamic
+      title: CardProfile(userModel: dummyUser),
+      centerTitle: false,
+      titleSpacing: 0,
+      toolbarHeight: 95.91,
+      forceElevated: innerBoxIsScrolled,
+    );
+  }
+}
