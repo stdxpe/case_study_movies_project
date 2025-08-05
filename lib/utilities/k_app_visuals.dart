41516e771297e2b3428371c5c81eda9e67cab2c0
Yasin Bilgin <stdxpe@gmail.com>
2025-08-05 19:26:30 +0300

    app wide visual names created

---
+++ lib/utilities/k_app_visuals.dart
@@ -0,0 +1,30 @@
+part of 'utilities_library_imports.dart';
+
+/// [APP WIDE GLOBAL VARIABLE]
+abstract class AppVisuals {
+  static const String google = 'assets/icons/google.svg';
+  static const String apple = 'assets/icons/apple.svg';
+  static const String facebook = 'assets/icons/facebook.svg';
+
+  static const String dummyProfilePic = 'assets/images/dummy_profile_pic.png';
+  static const String dummyMoviePic = 'assets/images/dummy_movie_pic.png';
+
+  static const String home = 'assets/icons/home.svg';
+  static const String profile = 'assets/icons/profile.svg';
+
+  static const String diamond = 'assets/icons/diamond.png';
+  static const String gem = 'assets/icons/gem.svg';
+  static const String heart = 'assets/icons/heart.png';
+  static const String hide = 'assets/icons/hide.svg';
+  static const String leftArrow = 'assets/icons/leftArrow.svg';
+  static const String like = 'assets/icons/like.svg';
+  static const String logo = 'assets/icons/logo.svg';
+  static const String mail = 'assets/icons/mail.svg';
+  static const String plus = 'assets/icons/plus.svg';
+  static const String twoHeart = 'assets/icons/twoHeart.png';
+  static const String unlock = 'assets/icons/unlock.svg';
+  static const String upArrow = 'assets/icons/upArrow.png';
+  static const String red = 'assets/icons/red.png';
+  static const String purple = 'assets/icons/purple.png';
+  static const String user = 'assets/icons/user.svg';
+}
