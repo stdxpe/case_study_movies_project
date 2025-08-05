beee11096899f2da79d1396f687189ef1405a3ea
Yasin Bilgin <stdxpe@gmail.com>
2025-08-05 19:29:03 +0300

    utility imports library created

---
+++ lib/utilities/utilities_library_imports.dart
@@ -0,0 +1,13 @@
+library utilities;
+
+import 'package:flutter/material.dart';
+
+/// All imports are utilized by the following sections
+part 'k_app_constants.dart';
+part 'k_app_strings.dart';
+part 'k_app_themes.dart';
+part 'k_app_visuals.dart';
+part 'k_color_palette.dart';
+part 'k_color_extensions.dart';
+part 'k_extensions.dart';
+part 'k_text_themes.dart';
