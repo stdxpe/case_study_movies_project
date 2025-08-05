1c0aa877e84f9c84c9662bfa088c1fa6d1e73a79
Yasin Bilgin <stdxpe@gmail.com>
2025-08-05 23:18:00 +0300

    swipeable movie card created

--- lib/ui/screens/discover_screen.dart
+++ lib/ui/screens/discover_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:case_study_movies_project/services/dummy_data.dart';
-import 'package:case_study_movies_project/ui/widgets/button_favorite.dart';
-import 'package:case_study_movies_project/ui/widgets/card_movie_description.dart';
-import 'package:case_study_movies_project/utilities/utilities_library_imports.dart';
+import 'package:case_study_movies_project/ui/widgets/card_movie_swipeable.dart';
 
 class DiscoverScreen extends StatefulWidget {
   const DiscoverScreen({super.key});
@@ -68,63 +66,7 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
               itemCount: moviesData.length,
               itemBuilder: (context, index) {
                 final movie = moviesData[index];
-                return Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    Image.network(
-                      movie.posterUrl,
-                      fit: BoxFit.cover,
-                      loadingBuilder: (context, child, loadingProgress) {
-                        if (loadingProgress == null) return child;
-                        return const Center(
-                          child: CircularProgressIndicator(
-                            color: Colors.white,
-                            strokeWidth: 2,
-                          ),
-                        );
-                      },
-                    ),
-                    Positioned(
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      height: AppConstants.sizes.gradientHeight,
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                            colors: [
-                              ColorPalette.permaBlack,
-                              ColorPalette.transparent,
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      right: 0,
-                      bottom: AppConstants.paddings.favoriteButtonBottom,
-                      child: Container(
-                        alignment: Alignment.centerRight,
-                        padding: EdgeInsets.only(
-                            right: AppConstants.paddings.favoriteButtonRight),
-                        child: ButtonFavorite(onPressed: () {}),
-                      ),
-                    ),
-                    Positioned(
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      child: CardMovieDescription(
-                        title: movie.title,
-                        description: movie.description,
-                        iconPath: AppVisuals.logo,
-                        onPressed: () {},
-                      ),
-                    ),
-                  ],
-                );
+                return CardMovieSwipeable(movie: movie);
               },
             ),
             if (_isLoadingMore)
---
+++ lib/ui/widgets/card_movie_swipeable.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import 'package:case_study_movies_project/models/movie_model.dart';
+import 'package:case_study_movies_project/ui/widgets/button_favorite.dart';
+import 'package:case_study_movies_project/ui/widgets/card_movie_description.dart';
+import 'package:case_study_movies_project/utilities/utilities_library_imports.dart';
+
+class CardMovieSwipeable extends StatelessWidget {
+  const CardMovieSwipeable({super.key, required this.movie});
+
+  final MovieModel movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Image.network(
+          movie.posterUrl,
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.white,
+                strokeWidth: 2,
+              ),
+            );
+          },
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          height: AppConstants.sizes.gradientHeight,
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  ColorPalette.permaBlack,
+                  ColorPalette.transparent,
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          right: 0,
+          bottom: AppConstants.paddings.favoriteButtonBottom,
+          child: Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.only(
+                right: AppConstants.paddings.favoriteButtonRight),
+            child: ButtonFavorite(onPressed: () {}),
+          ),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: CardMovieDescription(
+            title: movie.title,
+            description: movie.description,
+            iconPath: AppVisuals.logo,
+            onPressed: () {},
+          ),
+        ),
+      ],
+    );
+  }
+}
