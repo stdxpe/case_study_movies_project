18e9952e0716b8b85074d3122d8eec1dd1df30c5
Yasin Bilgin <stdxpe@gmail.com>
2025-08-06 11:36:07 +0300

    token storage service relocated

--- lib/services/global_services.dart/dependency_injection_service.dart
+++ lib/services/global_services.dart/dependency_injection_service.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'package:case_study_movies_project/services/abstract_classes/i_auth_service.dart';
 import 'package:case_study_movies_project/services/abstract_classes/i_logger_service.dart';
@@ -18,6 +18,14 @@ var locator = GetIt.instance;
 /// [DEPENDENCY INJECTION SERVICE]
 
 void initializeDependencyInjectionService() {
+  /// Flutter Secure Storage Instance Implementation
+  locator.registerLazySingleton<FlutterSecureStorage>(
+      () => const FlutterSecureStorage());
+
+  /// Token Storage Service (Data Access Layer)
+  locator
+      .registerLazySingleton<ITokenStorageService>(() => TokenStorageService());
+
   /// API Client Registered as Dio
   locator.registerSingleton(ApiClient());
   locator.registerSingleton(locator<ApiClient>().getDio());
@@ -30,14 +38,6 @@ void initializeDependencyInjectionService() {
 
   /// Movie Service (Data Access Layer)
   locator.registerLazySingleton<IMovieService>(() => NodeLabsMovieService());
-
-  /// Token Storage Service (Data Access Layer)
-  locator
-      .registerLazySingleton<ITokenStorageService>(() => TokenStorageService());
-
-  /// Flutter Secure Storage Instance Implementation
-  locator.registerLazySingleton<FlutterSecureStorage>(
-      () => const FlutterSecureStorage());
 
   /// Logger Service (Global Service)
   locator.registerLazySingleton<ILoggerService>(() => LoggerService());
