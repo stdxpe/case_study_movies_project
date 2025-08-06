8844bfbe02f4c58a8177c1f7475ac0f358efa41b
Yasin Bilgin <stdxpe@gmail.com>
2025-08-06 03:45:40 +0300

    dependency injector implemented to the app root

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'package:case_study_movies_project/services/global_services.dart/dependency_injection_service.dart';
+import 'package:case_study_movies_project/ui/screens/0test_screen.dart';
 import 'package:case_study_movies_project/ui/screens/main_screen_with_navbar.dart';
 import 'package:case_study_movies_project/utilities/utilities_library_imports.dart';
 
-void main() => runApp(const RootApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  initializeDependencyInjectionService();
+  runApp(const RootApp());
+}
 
 class RootApp extends StatelessWidget {
   const RootApp({super.key});
@@ -16,12 +22,9 @@ class RootApp extends StatelessWidget {
       darkTheme: AppThemes.dark,
       themeMode: ThemeMode.dark,
 
-      // home: const SignInScreen(),
-      // home: const SignUpScreen(),
-      // home: const UploadPhotosScreen(),
-      // home: const ProfileDetailsScreen(),
       debugShowCheckedModeBanner: false,
-      home: const HomeWithCustomNavButtons(),
+      // home: const HomeWithCustomNavButtons(),
+      home: TestScreen(),
     );
   }
 }
