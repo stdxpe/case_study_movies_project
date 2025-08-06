e256cf57b1b08b9c6b7158a735f4354caac5ee79
Yasin Bilgin <stdxpe@gmail.com>
2025-08-06 11:32:38 +0300

    content-type bearer implemented

--- lib/services/global_services.dart/api_client.dart
+++ lib/services/global_services.dart/api_client.dart
@@ -1,14 +1,15 @@
 import 'package:dio/dio.dart';
-
 import 'package:case_study_movies_project/services/abstract_classes/i_token_storage_service.dart';
 import 'package:case_study_movies_project/services/global_services.dart/dependency_injection_service.dart';
 import 'package:case_study_movies_project/utilities/utilities_library_imports.dart';
 
 class ApiClient {
   Dio getDio() {
-    Dio dio = Dio();
+    final dio = Dio();
+
     dio.options.baseUrl = AppConstants.baseUrl;
     dio.interceptors.add(AuthInterceptor());
+
     return dio;
   }
 }
@@ -21,10 +22,13 @@ class AuthInterceptor extends Interceptor {
       RequestOptions options, RequestInterceptorHandler handler) async {
     try {
       final token = await _tokenStorage.getToken();
+
       if (token != null && token.isNotEmpty) {
-        options.headers['Authorization'] = 'Bearer $token';
+        options.headers["Authorization"] = "Bearer $token";
+        options.headers["Content-Type"] = "application/json";
       }
     } catch (_) {}
+
     super.onRequest(options, handler);
   }
 }
