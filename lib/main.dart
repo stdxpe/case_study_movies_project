7dcd9e9e7e98187f97932c98d3ba86cd6a037c32
Yasin Bilgin <stdxpe@gmail.com>
2025-08-10 04:53:26 +0300

    firebase crashlytics implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,59 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
-import 'package:case_study_movies_project/services/global_services.dart/dependency_injection_service.dart';
-import 'package:case_study_movies_project/services/global_services.dart/navigation_service.dart';
+import 'package:case_study_movies_project/firebase_options.dart';
 import 'package:case_study_movies_project/ui/bloc/auth_bloc.dart';
-import 'package:case_study_movies_project/ui/bloc/auth_event.dart';
-import 'package:case_study_movies_project/ui/bloc/lottie_animation_cubit.dart';
-import 'package:case_study_movies_project/ui/bloc/movie_bloc.dart';
-import 'package:case_study_movies_project/ui/bloc/movie_event.dart';
 import 'package:case_study_movies_project/ui/bloc/user_bloc.dart';
 import 'package:case_study_movies_project/ui/bloc/user_event.dart';
+import 'package:case_study_movies_project/ui/bloc/auth_event.dart';
+import 'package:case_study_movies_project/ui/bloc/movie_bloc.dart';
+import 'package:case_study_movies_project/ui/bloc/movie_event.dart';
+import 'package:case_study_movies_project/ui/bloc/theme_cubit.dart';
+import 'package:case_study_movies_project/ui/bloc/lottie_animation_cubit.dart';
 import 'package:case_study_movies_project/utilities/utilities_library_imports.dart';
+import 'package:case_study_movies_project/services/global_services.dart/dependency_injection_service.dart';
+import 'package:case_study_movies_project/services/global_services.dart/navigation_service.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  /// DotEnv Implemented as Environment Variable Solution
+  await dotenv.load(fileName: AppConstants.envPath);
+
+  /// Firebase Implemented as Cloud Solution
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   /// GetIt Implemented as Dependency Injection Solution
   initializeDependencyInjectionService();
-  runApp(const RootApp());
+
+  /// EasyLocalization Implemented as Localization Service
+  await EasyLocalization.ensureInitialized();
+
+  /// Firebase Crashlytics Implemented as Remote Logger Service
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  runApp(
+    EasyLocalization(
+      path: Localization.path,
+      fallbackLocale: const Locale(Localization.turkish),
+      supportedLocales: const [
+        Locale(Localization.english),
+        Locale(Localization.turkish),
+      ],
+      child: const RootApp(),
+    ),
+  );
 }
 
 class RootApp extends StatelessWidget {
@@ -29,25 +65,38 @@ class RootApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider<AuthBloc>(
-            create: (_) => locator<AuthBloc>()..add(CheckAuthStatusEvent())),
+          create: (_) => locator<AuthBloc>()..add(CheckAuthStatusEvent()),
+        ),
         BlocProvider<UserBloc>(
-            create: (_) => locator<UserBloc>()..add(GetUserProfileEvent())),
+          create: (_) => locator<UserBloc>()..add(GetUserProfileEvent()),
+        ),
         BlocProvider(create: (_) => LottieAnimationCubit()),
+        BlocProvider<ThemeCubit>(create: (_) => ThemeCubit()),
         BlocProvider<MovieBloc>(
-            create: (_) => locator<MovieBloc>()
-              ..add(GetMoviesEvent(page: 1))
-              ..add(GetFavoriteMoviesEvent())),
+          create: (_) => locator<MovieBloc>()
+            ..add(GetMoviesEvent(page: 1))
+            ..add(GetFavoriteMoviesEvent()),
+        ),
       ],
-      child: MaterialApp.router(
-        title: AppStrings.appTitle,
-        themeMode: ThemeMode.dark,
-        darkTheme: AppThemes.dark,
-        theme: AppThemes.light,
+      child: BlocBuilder<ThemeCubit, ThemeMode>(builder: (context, themeMode) {
+        return MaterialApp.router(
+          title: AppConstants.appTitle,
 
-        /// Go_Router Implemented as Advanced Navigation Solution
-        routerConfig: AppRouter.router,
-        debugShowCheckedModeBanner: false,
-      ),
+          /// Custom Dark/Light Mode Themes Implemented
+          themeMode: themeMode,
+          darkTheme: AppThemes.dark,
+          theme: AppThemes.light,
+          themeAnimationDuration: const Duration(milliseconds: 10),
+
+          /// Go_Router Implemented as Advanced Navigation Solution
+          routerConfig: AppRouter.router,
+          localizationsDelegates: context.localizationDelegates,
+
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          debugShowCheckedModeBanner: false,
+        );
+      }),
     );
   }
 }
--- lib/services/global_services.dart/dependency_injection_service.dart
+++ lib/services/global_services.dart/dependency_injection_service.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-import 'package:case_study_movies_project/services/abstract_classes/i_auth_service.dart';
-import 'package:case_study_movies_project/services/abstract_classes/i_logger_service.dart';
-import 'package:case_study_movies_project/services/abstract_classes/i_movie_service.dart';
-import 'package:case_study_movies_project/services/abstract_classes/i_token_storage_service.dart';
-import 'package:case_study_movies_project/services/abstract_classes/i_user_service.dart';
-import 'package:case_study_movies_project/services/global_services.dart/api_client.dart';
-import 'package:case_study_movies_project/services/global_services.dart/navigation_redirect_service.dart';
-import 'package:case_study_movies_project/services/global_services.dart/navigation_service.dart';
-import 'package:case_study_movies_project/services/image_handler_service.dart';
-import 'package:case_study_movies_project/services/logger_service.dart';
-import 'package:case_study_movies_project/services/nodelabs_auth_service.dart';
-import 'package:case_study_movies_project/services/nodelabs_movie_service.dart';
-import 'package:case_study_movies_project/services/nodelabs_user_service.dart';
-import 'package:case_study_movies_project/services/token_storage_service.dart';
 import 'package:case_study_movies_project/ui/bloc/auth_bloc.dart';
 import 'package:case_study_movies_project/ui/bloc/movie_bloc.dart';
-import 'package:case_study_movies_project/ui/bloc/navigation_bar_cubit.dart';
 import 'package:case_study_movies_project/ui/bloc/user_bloc.dart';
+import 'package:case_study_movies_project/ui/bloc/navigation_bar_cubit.dart';
+import 'package:case_study_movies_project/services/image_handler_service.dart';
+import 'package:case_study_movies_project/services/nodelabs_auth_service.dart';
+import 'package:case_study_movies_project/services/nodelabs_user_service.dart';
+import 'package:case_study_movies_project/services/token_storage_service.dart';
+import 'package:case_study_movies_project/services/nodelabs_movie_service.dart';
+import 'package:case_study_movies_project/services/firebase_logger_service.dart';
+import 'package:case_study_movies_project/services/global_services.dart/api_client.dart';
+import 'package:case_study_movies_project/services/abstract_classes/i_auth_service.dart';
+import 'package:case_study_movies_project/services/abstract_classes/i_movie_service.dart';
+import 'package:case_study_movies_project/services/abstract_classes/i_user_service.dart';
+import 'package:case_study_movies_project/services/abstract_classes/i_logger_service.dart';
+import 'package:case_study_movies_project/services/abstract_classes/i_token_storage_service.dart';
+import 'package:case_study_movies_project/services/global_services.dart/navigation_service.dart';
+import 'package:case_study_movies_project/services/global_services.dart/navigation_redirect_service.dart';
 
 var locator = GetIt.instance;
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
@@ -27,7 +29,7 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 /// [DEPENDENCY INJECTION SERVICE]
 
 void initializeDependencyInjectionService() {
-  /// Flutter Secure Storage Instance Implementation
+  ////// Flutter Secure Storage Implemented
   locator.registerLazySingleton<FlutterSecureStorage>(() {
     return const FlutterSecureStorage();
   });
@@ -55,8 +57,13 @@ void initializeDependencyInjectionService() {
     return ImageHandlerService();
   });
 
-  /// Logger Service (Global Service)
-  locator.registerLazySingleton<ILoggerService>(() => LoggerService());
+  /// Firebase Logger Service (Crashlytics & Analytics)
+  locator.registerLazySingleton<ILoggerService>(() =>
+      FirebaseLoggerService(analytics: locator(), crashlytics: locator()));
+  locator.registerLazySingleton<FirebaseAnalytics>(
+      () => FirebaseAnalytics.instance);
+  locator.registerLazySingleton<FirebaseCrashlytics>(
+      () => FirebaseCrashlytics.instance);
 
   /// All Bloc's Registered with Service Class Constructors via GetIt DI
   locator.registerSingleton<AuthBloc>(
