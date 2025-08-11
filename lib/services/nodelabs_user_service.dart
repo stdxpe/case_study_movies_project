7ad66acda4f54b164696c76da63eafedc41c0937
Yasin Bilgin <stdxpe@gmail.com>
2025-08-11 04:43:15 +0300

    logger implemented on getprofile and uploadphoto methods

--- lib/services/nodelabs_user_service.dart
+++ lib/services/nodelabs_user_service.dart
@@ -16,25 +16,28 @@ class NodeLabsUserService extends IUserService {
     const String endPoint = '/user/profile';
 
     try {
-      _logger.logInfo('Fetching user profile from $endPoint');
+      _logger.logInfo('GET $endPoint → Fetching profile');
       final response = await _dio.get(endPoint);
 
       if (response.statusCode == 200) {
-        _logger.logInfo('User profile fetched successfully');
+        _logger.logInfo('Successfully fetched profile from $endPoint');
+
         final profileResponse = UserProfileResponseModel.fromMap(response.data);
         return profileResponse.user;
       } else if (response.statusCode == 401) {
-        _logger.logWarning('Unauthorized access when fetching profile');
+        _logger.logError(
+            'GET $endPoint → ${AppStrings.errors.unauthorized401}: ${response.statusCode}');
         throw Exception(
             '${AppStrings.errors.unauthorized401}: ${response.statusCode}');
       } else {
         _logger.logError(
-            'Failed to fetch user profile with status code ${response.statusCode}');
+            'GET $endPoint → ${AppStrings.errors.unknown}: ${response.statusCode}');
+
         throw Exception(
             '${AppStrings.errors.profileFail}: ${response.statusCode}');
       }
-    } on DioException catch (e, stack) {
-      _logger.logError('DioException in getProfile', error: e, stack: stack);
+    } on DioException catch (error, stacktrace) {
+      _logger.logError('GET $endPoint → Exception: $error $stacktrace');
       rethrow;
     }
   }
@@ -44,35 +47,37 @@ class NodeLabsUserService extends IUserService {
     const String endPoint = '/user/upload_photo';
 
     try {
-      _logger.logInfo('Uploading photo to $endPoint');
       final file = await MultipartFile.fromFile(filePath);
       final formData = FormData.fromMap({'file': file});
 
+      _logger.logInfo('Uploading photo to $endPoint');
       final response = await _dio.post(endPoint, data: formData);
 
-      _logger.logInfo('Upload photo response status: ${response.statusCode}');
       if (response.statusCode == 200) {
         final uploadResponse =
             UploadPhotoResponseModel.fromMap(response.data['data']);
         _logger.logInfo(
-            'Photo uploaded successfully, url: ${uploadResponse.photoUrl}');
+            'Successfully uploaded ${uploadResponse.photoUrl} to $endPoint');
         return uploadResponse.photoUrl;
       } else if (response.statusCode == 400) {
-        _logger.logWarning('Invalid file format for photo upload');
+        _logger.logWarning(
+            'POST $endPoint → ${AppStrings.errors.uploadPhotoInvalidFormat}: ${response.statusCode}');
         throw Exception(
             '${AppStrings.errors.uploadPhotoInvalidFormat}: ${response.statusCode}');
       } else if (response.statusCode == 401) {
-        _logger.logWarning('Unauthorized access when uploading photo');
+        _logger.logError(
+            'POST $endPoint → ${AppStrings.errors.unauthorized401}: ${response.statusCode}');
         throw Exception(
             '${AppStrings.errors.unauthorized401}: ${response.statusCode}');
       } else {
         _logger.logError(
-            'Photo upload failed with status code ${response.statusCode}');
+            'POST $endPoint → ${AppStrings.errors.unknown}: ${response.statusCode}');
+
         throw Exception(
             '${AppStrings.errors.photoServerUploadFail}: ${response.statusCode}');
       }
-    } on DioException catch (e, stack) {
-      _logger.logError('DioException in uploadPhoto', error: e, stack: stack);
+    } on DioException catch (error, stacktrace) {
+      _logger.logError('GET $endPoint → Exception: $error $stacktrace');
       rethrow;
     }
   }
