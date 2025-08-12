b42fb00536d0ab3f1b83b5e24c5cb2b0cb89f121
Yasin Bilgin <stdxpe@gmail.com>
2025-08-12 11:02:58 +0300

    api endpoints and baseurl constants created

---
+++ lib/utilities/k_endpoints.dart
@@ -0,0 +1,12 @@
+abstract class ApiEndpoints {
+  static const String baseUrl = "https://caseapi.servicelabs.tech/";
+
+  static const String login = "/user/login";
+  static const String register = "/user/register";
+  static const String profile = "/user/profile";
+  static const String uploadPhoto = "/user/upload_photo";
+
+  static const String movies = "/movie/list";
+  static const String favorites = "/movie/favorites";
+  static const String favoriteWithId = "/movie/favorites/";
+}
