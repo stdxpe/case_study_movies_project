a8520880765d647cffa81a491c9965a34f52a343
Yasin Bilgin <stdxpe@gmail.com>
2025-08-12 11:32:03 +0300

    interceptor class separated from client

--- lib/services/global_services/api_client.dart
+++ lib/services/global_services/api_client.dart
@@ -1,34 +1,18 @@
 import 'package:dio/dio.dart';
-import 'package:case_study_movies_project/services/abstract_classes/i_token_storage_service.dart';
-import 'package:case_study_movies_project/services/global_services/dependency_injection_service.dart';
-import 'package:case_study_movies_project/utilities/utilities_library_imports.dart';
+import 'package:case_study_movies_project/utilities/k_endpoints.dart';
+import 'package:case_study_movies_project/services/global_services/auth_interceptor.dart';
 
 class ApiClient {
+  final AuthInterceptor authInterceptor;
+
+  ApiClient({required this.authInterceptor});
+
   Dio getDio() {
     final dio = Dio();
 
-    dio.options.baseUrl = AppConstants.baseUrl;
-    dio.interceptors.add(AuthInterceptor());
+    dio.options.baseUrl = ApiEndpoints.baseUrl;
+    dio.interceptors.add(authInterceptor);
 
     return dio;
-  }
-}
-
-class AuthInterceptor extends Interceptor {
-  final _tokenStorage = locator<ITokenStorageService>();
-
-  @override
-  void onRequest(
-      RequestOptions options, RequestInterceptorHandler handler) async {
-    try {
-      final token = await _tokenStorage.getToken();
-
-      if (token != null && token.isNotEmpty) {
-        options.headers["Authorization"] = "Bearer $token";
-        options.headers["Content-Type"] = "application/json";
-      }
-    } catch (_) {}
-
-    super.onRequest(options, handler);
   }
 }
